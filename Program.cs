35336b15a0aad13279a1214a5c3fc82927a41263
Hawksprey <75325547+Hawksprey420@users.noreply.github.com>
2022-09-08 14:47:36 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Security;
+
+namespace Program
+{
+    class Check
+    {
+        static void Main (string[] args)
+        {
+            string myText = "Hello World";
+            int myNum = 15;
+            double myDoubleNum = 8.99;
+            char myLetter = 'F';
+            bool myBool = false;
+
+            Console.WriteLine (myText);
+            Console.WriteLine (myNum);
+            Console.WriteLine (myDoubleNum);
+            Console.WriteLine (myLetter);
+            Console.WriteLine (myBool);
+        }
+
+    }
+}
